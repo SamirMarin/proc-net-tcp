0054b47371264668802f16539b89759bc3428c9b
Samir Marin <smarin@skillz.com>
2022-05-13 23:16:20 -0700

    Convert ip and ports read form /proc/net/tcp to port place the converted ip:port connections into a connection map

--- pkg/tcp/tcp.go
+++ pkg/tcp/tcp.go
@@ -3,11 +3,25 @@ package tcp
 import (
 	"fmt"
 	"io/ioutil"
+	"math/big"
 	"strings"
+	"time"
 )
 
-type Client struct {
-	Connections []ProcNetTcp
+type Tcp struct {
+	TimeStamp   time.Time
+	Connections map[string]ProcNetTcp
+	PortScan    PortScan
+}
+
+type PortScan struct {
+	sourceIP string
+	port     []Port
+}
+
+type Port struct {
+	TimeStamp time.Time
+	Port      string
 }
 
 type ProcNetTcp struct {
@@ -15,37 +29,43 @@ type ProcNetTcp struct {
 	LocalPort    string
 	RemoteAdress string
 	RemotePort   string
-	Inode        string
 }
 
-func Connections(filepath string) (*Client, error) {
-	connectionsRead, err := readFile(filepath)
+// returns a new tcp connection
+func Connections(filepath string) (*Tcp, error) {
+	connectionsRead, err := readProcNetTcpFile(filepath)
 	if err != nil {
 		return nil, err
 	}
-	fmt.Println(connectionsRead)
 
+	connections := map[string]ProcNetTcp{}
 	for _, connection := range connectionsRead {
-		fmt.Println(connection)
-		for _, entry := range connection {
-			fmt.Println(entry)
+		ipLoc, portLoc := hexToStringIPPort(connection[1])
+		ipRem, portRem := hexToStringIPPort(connection[2])
+		key := fmt.Sprintf("%s:%s-%s:%s", ipLoc, portLoc, ipRem, portRem)
+
+		connections[key] = ProcNetTcp{
+			LocalAdress:  ipLoc,
+			LocalPort:    portLoc,
+			RemoteAdress: ipRem,
+			RemotePort:   portRem,
 		}
 	}
+	fmt.Println(connections)
 
-	connections := []ProcNetTcp{}
-
-	return &Client{
+	return &Tcp{
 		Connections: connections,
 	}, nil
 }
 
-func readFile(path string) ([][]string, error) {
+// Reads /proc/net/tcp file
+func readProcNetTcpFile(path string) ([][]string, error) {
 	tcpConncetionsFile, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
 
-	entries := strings.Split(string(tcpConncetionsFile), "\n")
+	entries := strings.Split(string(tcpConncetionsFile), "\n")[1:]
 
 	entriesClean := [][]string{}
 	for _, entry := range entries {
@@ -58,7 +78,39 @@ func readFile(path string) ([][]string, error) {
 				entryClean = append(entryClean, entryVal)
 			}
 		}
-		entriesClean = append(entriesClean, entryClean)
+		if len(entryClean) > 0 {
+			entriesClean = append(entriesClean, entryClean)
+		}
 	}
 	return entriesClean, nil
+}
+
+// Get ip, port from hex: ip:port
+func hexToStringIPPort(ipPort string) (string, string) {
+	ipPortArr := strings.Split(ipPort, ":")
+	ip := ipHexDecStr(ipPortArr[0])
+	port := hexToDec(ipPortArr[1])
+
+	return ip, port
+}
+
+// Converts hex to dec
+func hexToDec(hex string) string {
+	n := new(big.Int)
+	n.SetString(hex, 16)
+
+	return n.String()
+}
+
+// Get IP in decimal from hex given in little endian
+func ipHexDecStr(ip string) string {
+	ipStr := ""
+	for i, _ := range ip {
+		if i%2 == 1 {
+			ipStr = hexToDec(ip[i-1:i+1]) + ipStr
+		} else if i != 0 {
+			ipStr = "." + ipStr
+		}
+	}
+	return ipStr
 }
