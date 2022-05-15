27d3d1663d56dc097ca9d3693ce6b8bdd680d103
Samir Marin <smarin@skillz.com>
2022-05-15 15:31:55 -0700

    Add prometheus metric counts total number of new connections

--- cmd/tcp/main.go
+++ cmd/tcp/main.go
@@ -4,13 +4,15 @@ import (
 	"proc-net-tcp/handlers"
 
 	"github.com/labstack/echo/v4"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
+	go handlers.Tcp()
+
 	e := echo.New()
 	e.GET("/health", handlers.Health)
-
-	go handlers.Tcp()
+	e.GET("/metrics", echo.WrapHandler(promhttp.Handler()))
 
 	// start server
 	e.Logger.Fatal(e.Start(":8080"))
